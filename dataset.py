dc3a34b6033ae3659d5a656df87e8650d857a15f
Lakshmi <s8laraje@stud.uni-saarland.de>
2021-01-10 15:42:21 +0100

    First commit LSTM model

---
+++ dataset.py
@@ -0,0 +1,45 @@
+import torch
+from torchtext import data
+from torchtext import datasets
+import random
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+SEED = 1234
+
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+TEXT = data.Field(tokenize = 'spacy', include_lengths = True)
+LABEL = data.LabelField(dtype = torch.float)
+
+def data_loaders(batch, device, embedding=True):
+    MAX_VOCAB_SIZE = 25_000
+    BATCH_SIZE = batch
+    
+    train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
+    train_data, valid_data = train_data.split(random_state = random.seed(SEED))
+    if embedding:
+        TEXT.build_vocab(train_data, 
+                        max_size = MAX_VOCAB_SIZE, 
+                        vectors = "glove.6B.100d", 
+                        unk_init = torch.Tensor.normal_)
+    else:
+        TEXT.build_vocab(train_data, 
+                        max_size = MAX_VOCAB_SIZE)
+
+    LABEL.build_vocab(train_data)
+    train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+        (train_data, valid_data, test_data), 
+        batch_size = BATCH_SIZE,
+        sort_key = lambda x: len(x.text),
+        sort_within_batch=True,
+        device = device)
+    
+    return (train_iterator, valid_iterator, test_iterator)
+
+def get_vocab():
+    return TEXT
+
+if __name__ == "__main__":
+    a,b,c = train_loaders(64, embedding=False)
---
+++ utils.py
@@ -0,0 +1,15 @@
+import torch
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division 
+    acc = correct.sum() / len(correct)
+    return acc
+
+# def rank_words(model, sent):
+#     for word in sent
